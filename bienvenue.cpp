94d067ee04a9ae3c4e81d1252dee57080fb5dc4c
drghassen <dridighassenbac2021@gmail.com>
2026-02-10 22:20:45 +0100

    Ajout du fichier README.md

--- bienvenue.cpp
+++ bienvenue.cpp
@@ -1,6 +1,9 @@
+#include <iostream>
+
 // TODO Indiquer ce que fait le programme
 int main()
 {
+    std::cout << "Bienvenue le monde !" << std::endl;
     // TODO Afficher un message de bienvenue
     return 0;
 }
